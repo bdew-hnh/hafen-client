37a4c67a461528239c30fba6b846394c3e24ad6e
bdew <blackdew@gmail.com>
2015-09-17 11:44:50 +0300

    Store config settings value instead of fetching them on every call

--- src/haven/ConfigSettingBoolean.java
+++ src/haven/ConfigSettingBoolean.java
@@ -3,19 +3,20 @@ package haven;
 public class ConfigSettingBoolean {
 	public String id;
 	public String name;
-	public boolean defaultState;
+	public boolean enabled;
 
 	public ConfigSettingBoolean(String id, String name, boolean defaultState) {
 		this.id = id;
 		this.name = name;
-		this.defaultState = defaultState;
+		this.enabled = Utils.getprefb(id, defaultState);
 	}
 
 	public boolean isEnabled() {
-		return Utils.getprefb(id, defaultState);
+		return enabled;
 	}
 
-	public void setEnabled(Boolean state) {
+	public void setEnabled(boolean state) {
+		enabled = state;
 		Utils.setprefb(id, state);
 	}
 
