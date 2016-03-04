59265ff6660d116b6ea477851ee6dbf13ee08488
bdew <blackdew@gmail.com>
2016-03-04 06:27:28 +0200

    Removed stat gain notification, now in official client

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -462,12 +462,6 @@ public class CharWnd extends Window {
 
 	public void tick(double dt) {
 	    if((attr.base != cbv) || (attr.comp != ccv)) {
-        if (cbv != 0 && attr.base != cbv) {
-            int diff = attr.base - cbv;
-            GameUI ui = getparent(GameUI.class);
-            if (ui != null)
-                ui.msg(String.format("You've %s %d %s!", (diff > 0) ? "gained" : "lost", diff, rnm.text), Color.GREEN);
-        }
 		cbv = attr.base; ccv = attr.comp;
 		Color c = Color.WHITE;
 		if(ccv > cbv) {
